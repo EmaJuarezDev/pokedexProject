5fb41a3b5a5fd9b8347a5c86ec8b6f7534ebe326
naGGIX <joeeel.8@hotmail.com>
2021-08-18 19:09:12 -0300

    new
    bttn

--- script.js
+++ script.js
@@ -13,13 +13,16 @@ function pokemon() {
       let id = Math.floor(Math.random()*898)
       fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
      .then(response => response.json())
-     .then(data => {postImgPokemon(data.sprites.front_default,index)})
+     .then(data => {postImgPokemon(data.sprites.other["official-artwork"].front_default,index)})
     .catch(error => console.log(error))
         
     }
     
 }
 pokemon();
+document.getElementById('btn').addEventListener('click',()=> {location.reload().getElementById('_body')
+  pokemon();
+})
 /*async function postImgPokemon(imgUrl) {
     for (let index = 1; index <= 36 ; index++){
          document.getElementById(`img-main${index}`).setAttribute('src',`${imgUrl}`)
