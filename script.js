26c50b573780c0f88a377dfcf10fed2712239951
naGGIX <joeeel.8@hotmail.com>
2021-08-18 16:13:23 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+/*let postImgPokemon = async(img) => {
+    let element = document.getElementById('img-main1')
+    await element.setAttribute('src',`${img}`)
+}*/
+function postImgPokemon(img,num) {
+    document.getElementById(`img-main${num}`).setAttribute('src',`${img}`)
+}
+ 
+function pokemon() {
+    let arrayPokemon= [];
+    for (let index = 1; index <= 36; index++) {
+      let id = Math.floor(Math.random()*898)
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+     .then(response => response.json())
+     .then(data => {postImgPokemon(data.sprites.front_default,index)})
+    .catch(error => console.log(error))
+        
+    }
+    
+}
+pokemon();
+/*async function postImgPokemon(imgUrl) {
+    for (let index = 1; index <= 36 ; index++){
+         document.getElementById(`img-main${index}`).setAttribute('src',`${imgUrl}`)
+    }
+}    */
+
